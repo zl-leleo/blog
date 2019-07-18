e204c42fd6838f10871a13b2c35f5d872ac1154d
zl-leleo <44083191+zl-leleo@users.noreply.github.com>
2019-07-18 17:49:57 +0800

    Create users.js

---
+++ schemas/users.js
@@ -0,0 +1,45 @@
+
+var mongoose = require('mongoose');
+
+
+// 用户的表结构
+module.exports = new mongoose.Schema({
+  // 用户名
+  username: String,
+  // 密码
+  password: String
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
