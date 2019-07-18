1588714ed842c1321b3cdfe24f582b0fc2dfbf7c
zl-leleo <44083191+zl-leleo@users.noreply.github.com>
2019-07-18 17:50:58 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,15 @@ var express = require('express');
 // 加载模板处理模块
 var swig = require('swig');
 
+// 加载数据库模块
+var mongoose = require('mongoose');
+
 // 创建app应用 => nodejs http.createServer();
 var app = express();
+
+// 设置静态文件托管
+// 当用户访问的url的/public开始，那么直接返回对应__dirname + '/public' 下的文件
+app.use('/public', express.static( __dirname + '/public'));
 
 // 配置应用模板
 // 定义当前应用所使用的模板引擎
@@ -26,15 +33,27 @@ app.set('view engine', 'html');
 // req request对象
 // res response对象
 // next 函数
-app.get('/',function(req, res, next){
-  // 读取views目录下的置顶文件，解析并返回给客户端
-  // 第一个参数：表示模板的文件，相对于views目录， views/index.html
-  // 第二个参数：传递给模板使用的数据
-  res.render('index');
-});
+// app.get('/',function(req, res, next){
+//   // 读取views目录下的置顶文件，解析并返回给客户端
+//   // 第一个参数：表示模板的文件，相对于views目录， views/index.html
+//   // 第二个参数：传递给模板使用的数据
+//   res.render('index');
+// });
+
+// 根据不同的功能划分模块
+app.use('/admin', require('./routers/admin'));
+app.use('/api', require('./routers/api'));
+app.use('/', require('./routers/main'));
 
 // 监听请求
-app.listen('8081');
+mongoose.connect('mongodb://localhost:27017/blog',function(err){
+  if(err){
+    console.log('数据库连接失败')
+  }else{
+    console.log('数据库连接成功')
+    app.listen('8081');
+  }
+});
 
 
 
