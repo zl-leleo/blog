59485bc992a7fd100b8b94c3cb9d54f4ff34100b
zl-leleo <44083191+zl-leleo@users.noreply.github.com>
2019-07-19 17:48:27 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,12 +7,20 @@ var swig = require('swig');
 // 加载数据库模块
 var mongoose = require('mongoose');
 
+// 加载body-parser，用来处理post提交过来的数据
+var bodyParser = require('body-parser');
+
+// 加载cookies模块
+var Cookies = require('cookies');
+
 // 创建app应用 => nodejs http.createServer();
 var app = express();
 
+var User = require('./models/User');
+
 // 设置静态文件托管
 // 当用户访问的url的/public开始，那么直接返回对应__dirname + '/public' 下的文件
-app.use('/public', express.static( __dirname + '/public'));
+app.use('/public', express.static(__dirname + '/public'));
 
 // 配置应用模板
 // 定义当前应用所使用的模板引擎
@@ -22,11 +30,40 @@ app.engine('html', swig.renderFile);
 // 设置模板文件存放的目录，第一个参数必须是views，第二个参数是目录
 app.set('views', __dirname + '/views');
 
-// 开发过程中，需要取消模板缓存
-swig.setDefaults({cache:false});
 
 // 注册所使用的模板引擎，第一个参数必须是view engine，第二个参数和app.engine这个方法中定义的模板引擎的名称（第一个参数）保持一致
 app.set('view engine', 'html');
+
+// 开发过程中，需要取消模板缓存
+swig.setDefaults({ cache: false });
+
+// bodyparser设置
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// 设置cookies
+app.use(function(req, res, next) {
+  req.cookies = new Cookies(req, res);
+
+  // 解析登录用户的cookies信息
+  req.userInfo = {};
+  if (req.cookies.get('userInfo')) {
+    try {
+      req.userInfo = JSON.parse(req.cookies.get('userInfo'));
+
+      // 获取当前登录用户的类型，是否是管理员
+      User.findById(req.userInfo._id).then(function(userInfo) {
+        req.userInfo.isAdmin = Boolean(userInfo.isAdmin);
+        next();
+      })
+    } catch (e) {
+      next();
+    }
+
+  } else {
+    next();
+  }
+
+});
 
 
 // 首页
@@ -46,17 +83,11 @@ app.use('/api', require('./routers/api'));
 app.use('/', require('./routers/main'));
 
 // 监听请求
-mongoose.connect('mongodb://localhost:27017/blog',function(err){
-  if(err){
+mongoose.connect('mongodb://localhost:27017/blog', function(err) {
+  if (err) {
     console.log('数据库连接失败')
-  }else{
+  } else {
     console.log('数据库连接成功')
     app.listen('8081');
   }
-});
-
-
-
-
-
-
+}, { useNewUrlParser: true });
